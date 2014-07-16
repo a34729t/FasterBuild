c5f8ed81002b9ade5092a1c77eabb88d0425081d
Nicolas Flacco <ncf1989@yahoo.com>
2014-07-16 00:26:56 -0700

    trying to make line item replace work

--- FasterBuild/FasterBuild.m
+++ FasterBuild/FasterBuild.m
@@ -74,20 +74,65 @@ static FasterBuild *sharedPlugin;
 {
     BOOL on = [[NSUserDefaults standardUserDefaults] boolForKey:FasterBuildKey];
     [self toggleMenu:!on];
-    
-    NSString *msg;
-    if (on) {
-        msg = @"On!";
-    } else {
-        msg = @"Off!";
-    }
-    NSAlert *alert = [NSAlert alertWithMessageText:msg defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
-    [alert runModal];
+    NSString *workspacePath = [self currentProjectPath];
+
     
     // TODO: Run an NSTask to modify project on a line-item basis
+    // 1. Print out the path of the current project's project files
     
     
     
+    // 2. Do line item changes
+    if (on) {
+        [self lineItemReplace:@"DEBUG_INFORMATION_FORMAT = \"dwarf-with-dsym\";" with:@"DEBUG_INFORMATION_FORMAT = \"dwarf\";" path:workspacePath];
+    } else {
+        [self lineItemReplace:@"DEBUG_INFORMATION_FORMAT = \"dwarf\";" with:@"DEBUG_INFORMATION_FORMAT = \"dwarf-with-dsym\";" path:workspacePath];
+    }
+    
+//    NSString *msg;
+//    if (on) {
+//        msg = @"On!";
+//    } else {
+//        msg = @"Off!";
+//    }
+//    NSAlert *alert = [NSAlert alertWithMessageText:msg defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+//    [alert runModal];
+    
+}
+
+- (void)lineItemReplace:(NSString *)old with:(NSString *)new path:(NSString *)workspacePath
+{
+    // In theory, trying to do this
+    // http://stackoverflow.com/questions/24774122/nstask-calling-perl-and-piping-in-find-not-working
+    
+    // But it doesn't work!
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/bin/sh"];
+    NSString *regex = [NSString stringWithFormat:@"'s/%@/%@/g'", old, new];
+    NSString *pathArg = [NSString stringWithFormat:@"`find %@ -name *.pbxproj`", workspacePath];
+    [task setArguments:@[ @"/usr/bin/perl", @"-p", @"-i", @"-e", regex, pathArg]];
+    [task launch];
+}
+
+- (NSString *)currentProjectPath
+{
+    // See http://stackoverflow.com/questions/21054699/get-the-path-of-current-project-opened-in-xcode-plugin
+    
+    NSArray *workspaceWindowControllers = [NSClassFromString(@"IDEWorkspaceWindowController") valueForKey:@"workspaceWindowControllers"];
+    
+    id workSpace;
+    for (id controller in workspaceWindowControllers) {
+        if ([[controller valueForKey:@"window"] isEqual:[NSApp keyWindow]]) {
+            workSpace = [controller valueForKey:@"_workspace"];
+        }
+    }
+    
+    NSString *workspacePath = [[workSpace valueForKey:@"representingFilePath"] valueForKey:@"_pathString"];
+    
+    // This gets you something like:
+    //  /Users/nflacco/Projects/ios/alcatraz/FasterBuild/FasterBuild.xcodeproj
+    
+    return workspacePath;
 }
 
 - (void)dealloc
